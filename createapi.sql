d417669e3b6883f77fdbcf0a48cce7ea6f21252b
Kamshory, MT <kamshory@yahoo.com>
2018-11-13 08:04:54 +0700

    Update createapi.sql

--- createapi.sql
+++ createapi.sql
@@ -1,3 +1,3 @@
-INSERT INTO `push_api` (`api_id`, `api_key`, `hash_password_client`, `hash_password_pusher`, `active`) VALUES
-(NULL, 'PLANETBIRU', sha1('passclient'), sha1('passpusher'), 1);
+INSERT INTO `push_api` (`api_key`, `hash_password_client`, `hash_password_pusher`, `active`) VALUES
+('PLANETBIRU', sha1('passclient'), sha1('passpusher'), 1);
 
