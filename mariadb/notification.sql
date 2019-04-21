abd2eebc2eb3b8025c7edbbc4d99b0f7ceb51991
Kamshory, MT <kamshory@yahoo.com>
2019-04-21 20:58:42 +0700

    Update notification.sql

--- mariadb/notification.sql
+++ mariadb/notification.sql
@@ -125,7 +125,7 @@ CREATE TABLE `push_trash` (
  PRIMARY KEY (`trash_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
- 	CREATE TABLE `push_user` (
+CREATE TABLE `push_user` (
  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
  `api_id` bigint(20) DEFAULT NULL,
  `username` varchar(100) DEFAULT NULL,
@@ -155,4 +155,4 @@ CREATE TABLE `push_trash` (
  `active` tinyint(1) DEFAULT '1',
  PRIMARY KEY (`user_id`),
  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
